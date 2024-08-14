109ed4c15ac0262378c85b848503891dd3153d35
Kentaro Kusumi <35834896+kken7231@users.noreply.github.com>
2024-08-14 09:50:23 +0900

    add: powerlogger grafana

--- src/powerLogger/powerLogger/powerLogger.ino
+++ src/powerLogger/powerLogger/powerLogger.ino
@@ -1,23 +1,29 @@
 #include <Adafruit_INA260.h>
 #include "WiFi.h"
 #include <PubSubClient.h>
+#include "time.h"
 
 Adafruit_INA260 ina260 = Adafruit_INA260();
-int loggingInterval;
-long lastRead;
+int loggingInterval = 1000;
+long lastRead = 0;
 float ss;
 // WiFi
 const char *WIFI_SSID = "aBuffalo-T-E510";
 const char *WIFI_PASSWORD = "penguink";
 WiFiClient espClient;
-
-// MQTTブローカー
-const char *mqtt_broker = "192.168.11.16";
-const char *topic = "powerlogger";
-const int mqtt_port = 31883;
 PubSubClient client(espClient);
 
-char buffer[50];
+// Define NTP Client to get time
+const char* ntpServer = "pool.ntp.org";
+const long gmtOffset_sec = 3600 * 9;  // GMT offset for Japan Standard Time (JST)
+const int daylightOffset_sec = 0;
+
+// MQTT broker details
+const char* mqtt_broker = "192.168.11.16";
+const char* topic = "watts_data";
+const int mqtt_port = 31883;
+
+char buffer[100];
 
 void setup() {
   Serial.begin(115200);
@@ -25,6 +31,7 @@ void setup() {
   while (!Serial) { delay(10); }
   Serial.println("Serial Connected");
   
+  // Connect to WiFi
   while (WiFi.status() != WL_CONNECTED) {
     WiFi.mode(WIFI_STA);
     WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
@@ -32,6 +39,7 @@ void setup() {
   }
   Serial.println("WiFi Connected");
 
+  // Initialize INA260 sensor
   if (!ina260.begin()) {
     Serial.println("Couldn't find INA260 chip");
     while (1);
@@ -42,24 +50,36 @@ void setup() {
   ina260.setCurrentConversionTime(INA260_TIME_8_244_ms);
   ina260.setVoltageConversionTime(INA260_TIME_8_244_ms);
 
-  loggingInterval = 1000;
-  lastRead = 0;
   client.setServer(mqtt_broker, mqtt_port);
+
+  // Initialize and synchronize time with NTP server
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+
+  Serial.println("Setup complete");
 }
 
 void loop() {
   long diff;
   diff = millis() - lastRead;
 
-  if(diff >= loggingInterval){
+  if (diff >= loggingInterval) {
     lastRead += loggingInterval;
 
     float cur = ina260.readCurrent();
     float vol = ina260.readBusVoltage();
     float pow = ina260.readPower();
 
-    if(client.connected() || client.connect("abc")) {
-      sprintf(buffer, "{cur:%f,vol:%f,pow:%f}", cur, vol, pow);
+    if (client.connected() || client.connect("abc")) {
+      // Get the current time in seconds since the Unix epoch
+      time_t now;
+      struct tm timeinfo;
+      if (!getLocalTime(&timeinfo)) {
+        Serial.println("Failed to obtain time");
+        return;
+      }
+      time(&now);
+    
+      sprintf(buffer, "{\"ts\":%lu,\"cur\":%f,\"vol\":%f,\"pow\":%f}", (unsigned long)now, cur, vol, pow);
       Serial.println(buffer);
       client.publish(topic, buffer);
     }
