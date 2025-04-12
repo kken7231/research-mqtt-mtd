d0d2c7d16b3d127d499a15ab3f1aa2d15f25b0e3
Kentaro Kusumi <35834896+kken7231@users.noreply.github.com>
2025-04-12 02:37:37 +0000

    save changes

---
+++ rust-mqttmtd/authserver/src/main.rs
@@ -0,0 +1,15 @@
+use std::net::TcpStream;
+
+extern crate libmqttmtd;
+
+// fn issuer_handler(stream: TcpStream) {
+//     return;
+// }
+
+// fn verifier_handler(stream: TcpStream) {
+//     return;
+// }
+
+fn main() {
+    println!("Hello, world!");
+}
